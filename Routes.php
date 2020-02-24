30d109902e26ebba71e5b63b55e0e16fbcad1e32
Yakov32 <yakov.zverev32@gmail.com>
2020-02-24 18:08:23 +0300

    MVC
    Yakov  added simple MVC

---
+++ Routes.php
@@ -0,0 +1,11 @@
+<?php
+	Route::set("about_us", function() {
+		echo "about us";
+	});
+
+
+	Route::set("content_page", function() {
+		echo "dsadasdas";
+	});
+
+?>
---
+++ includes/classes/Route.php
@@ -0,0 +1,12 @@
+<?php
+	
+	class Route {
+
+		public static $validRoutes = [];
+		public static function set($route, $function){
+
+			self::$validRoutes[] = $route;
+			print_r(self::$validRoutes);	
+		}
+	} 
+ ?>
