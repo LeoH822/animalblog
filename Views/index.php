0c8e367327e5ba47ba89de3006b9999e2366b521
Leon Horne <leonhornejr@gmail.com>
2020-02-26 21:19:29 +0200

    Home page content added

--- Views/index.php
+++ Views/index.php
@@ -54,8 +54,8 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     <div class="w3-twothird w3-container">
       <h1 class="w3-text-teal">Articles</h1>
       <p>Welcome to the Animal Blog, below you will find articles all about animals.</p>
-    </div>
-    <img src="./static/images/logo.jpg" width="100" alt="howCode" />
+      <img src="static\Images\panda.png" alt="Panda Image"/>
+    </div>1
     <div class="w3-third w3-container">
       <p class="w3-border w3-padding-large w3-padding-32 w3-center">Image 1</p>
       <p class="w3-border w3-padding-large w3-padding-64 w3-center">Image 1</p>
@@ -66,6 +66,33 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     <div class="w3-twothird w3-container">
       <h2 class="w3-text-teal">Lion</h1>
       <p>A muscular, deep-chested cat with a short, rounded head, a reduced neck and round ears, and a hairy tuft at the end of its tail.</p>
+
+      <h2 class="w3-text-teal">Panda</h1>
+      <p>The giant panda, also known as the panda bear or simply the panda, is a bear native to south central China. It is characterised by large, black patches around its eyes, over the ears, and across its round body.</p>
+
+      <h2 class="w3-text-teal">Sloth</h1>
+      <p>Sloths of the present day are arboreal mammals noted for slowness of movement and for spending most of their lives hanging upside down in the trees of the tropical rain forests of South America and Central America.</p>
+
+      <h2 class="w3-text-teal">Tiger</h1>
+      <p>The tiger (Panthera tigris) is the largest species among the Felidae and classified in the genus Panthera. It is most recognisable for its dark vertical stripes on orangish-brown fur with a lighter underside. It is an apex predator, primarily preying on ungulates such as deer and wild boar.</p>
+
+      <h2 class="w3-text-teal">Wild Dog</h1>
+      <p>The African wild dog is a canid native to sub-Saharan Africa. It is the largest indigenous canid in Africa, and the only extant member of the genus Lycaon, which is distinguished from Canis by dentition highly specialised for a hypercarnivorous diet, and a lack of dewclaws.</p>
+
+      <h2 class="w3-text-teal">Elephant</h1>
+      <p>Elephants are mammals of the family Elephantidae and the largest existing land animals. Three species are currently recognised: the African bush elephant, the African forest elephant, and the Asian elephant.</p>
+
+      <h2 class="w3-text-teal">Pig</h1>
+      <p>A muscular, deep-chested cat with a short, rounded head, a reduced neck and round ears, and a hairy tuft at the end of its tail.</p>
+      
+      <h2 class="w3-text-teal">Fox</h1>
+      <p>Foxes are small-to-medium-sized, omnivorous mammals belonging to several genera of the family Canidae. Foxes have a flattened skull, upright triangular ears, a pointed, slightly upturned snout, and a long bushy tail (or brush).</p>
+
+      <h2 class="w3-text-teal">Racoon</h1>
+      <p>The raccoon (/rəˈkuːn/ or US: /ræˈkuːn/ (About this soundlisten), Procyon lotor), sometimes spelled racoon,[3] also known as the common raccoon,[4] North American raccoon,[5] northern raccoon,[6] or coon, is a medium-sized mammal native to North America. </p>
+
+      <h2 class="w3-text-teal">Honey Badger</h1>
+      <p>The honey badger (Mellivora capensis), also known as the ratel (/ˈreɪtəl/ or /ˈrɑːtəl/), is a mammal widely distributed in Africa, Southwest Asia, and the Indian subcontinent.</p>
     </div>
     <div class="w3-third w3-container">
       <p class="w3-border w3-padding-large w3-padding-32 w3-center">Image 1</p>
