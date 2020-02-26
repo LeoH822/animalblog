b24fc6cf0a6d69cf7916381e8c672b41b8d5107b
Yakov32 <yakov.zverev32@gmail.com>
2020-02-26 21:09:43 +0300

    addedTemplate
    Template.

--- includes/views/Index.php
+++ includes/views/Index.php
@@ -1,1 +1,45 @@
-<h1>HomePage</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Blog</title>
+	
+	<link href="https://fonts.googleapis.com/css?family=Kalam:300,400|Roboto:300i,400,500i|Trade+Winds&display=swap&subset=cyrillic" rel="stylesheet">
+	
+	<link rel="stylesheet" type="text/css" href="includes/css/style.css">
+
+</head>
+<body>
+
+
+
+	<header>
+		
+		<div id = "logoHeader"><img src="./includes/images/logo.png"></div>
+		
+		<div id = "menuHeader">
+			<p class = "menu"><a href="vk.com">HOME</a></p>
+			<p class = "menu"><a href="vk.com">ABOUT</a></p>
+			<p class = "menu"><a href="vk.com">CONTACT</a></p>
+			<p class = "menu"><a href="vk.com">ARTICLES</a></p>
+		</div>
+
+		<div id = "RegAutho">
+			<div class="regautho reg">Reg</div>
+			<div class="regautho autho">Autho</div>
+		</div>
+	
+	</header>
+
+	<div id = "allContent">
+		<div class="leftColumn">
+			left
+		</div>
+
+		<div class="RightColumnt">
+			right
+		</div>
+	</div>
+	
+</body>
+</html>
